8ac50ea224435c9050db59a957657f6fd868918a
Sazidhul <sazisazidhul@gmail.com>
2025-10-02 11:39:43 +0600

    params,create dynamic path and render Dynamic Route

---
+++ src/Componesnts/Root/Root.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Header from '../Header/Header';
+import { Outlet } from 'react-router';
+import Footer from '../Footer/Footer';
+import SideBar from '../SideBar/SideBar';
+import './Root.css'
+
+const Root = () => {
+   return (
+      <div>
+         <Header></Header>
+         <div className='root-main'>
+           <SideBar></SideBar>
+            <Outlet></Outlet>
+         </div>
+         
+         <Footer></Footer>
+      </div>
+   );
+};
+
+export default Root;
+
+
+/**
+ * Data loading How to Load the data 
+ * 1. Use: usersPromise > Suspense > promise > use(usersPromise)
+ * 2. [less used so far]   > useState + useEffect (() => {},[])
+ * 3. set loader in the route definition: load data before router component is rendered
+ * 
+ */
---
+++ src/main.jsx
@@ -0,0 +1,65 @@
+import { StrictMode, Suspense } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from './Componesnts/Root/Root.jsx';
+import Mobiles from './Componesnts/Mobiles/Mobiles.jsx';
+import Home from './Componesnts/Home/Home.jsx';
+import Laptops from './Componesnts/Laptops/Laptops.jsx';
+import Users from './Componesnts/Users.jsx/Users.jsx';
+import Users2 from './Componesnts/Users2/Users2.jsx';
+import UserDetails from './Componesnts/UserDetails/UserDetails.jsx';
+
+const usersPromise = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+
+const router = createBrowserRouter([
+  {  
+    path: "/",
+    Component: Root,
+    children: [
+        {index: true, Component: Home},
+        {path: 'mobiles', Component: Mobiles},
+        {path: 'laptops', Component: Laptops},
+        {
+          path: 'users',
+          loader: () => fetch('https://jsonplaceholder.typicode.com/users'),
+          Component:Users
+        },
+        {
+          path: 'users2',
+          element: <Suspense fallback={<span>Loading</span>}>
+            <Users2 usersPromise={usersPromise}></Users2>
+          </Suspense>
+        },
+        {
+          path: 'users/:userId', 
+          loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
+          Component: UserDetails
+        }
+    ]
+  },
+  {
+    path: 'about',
+    element: <div>About me here</div>
+  },
+  {
+    path: 'blogs',
+    element: <div>All my blogs are here</div>
+  },
+  {
+    path: 'app',
+    Component: App
+  },
+  {
+    path: '/app2',
+    element: <App></App>
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
